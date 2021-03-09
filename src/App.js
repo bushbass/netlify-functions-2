0f9ade8df03ce0f76945ccb3134875cc4133fbbc
Alex Nielsen <alex.nielsen@gmail.com>
2021-03-09 02:10:23 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    // <div className="App">
+    //   <a href="./.netlify/functions/hello-world">say hello</a>
+    // </div>
+    <form class="form-example" action="/.netlify/functions/hello-world">
+      <label>
+        What’s your name?
+        <input name="name" />
+      </label>
+      <button class="button" type="submit">
+        Say hello!
+      </button>
+    </form>
+  );
+}
